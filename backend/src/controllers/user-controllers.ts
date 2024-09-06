b96af5800c590245787e35b465d3d347c6a8f19a
Sudheesh T D <sudheeshtd01@gmail.com>
2024-09-06 16:32:18 -0500

    User Route for Get all users

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,17 @@
-export const getAllUsers = () => {
-  //get  all users
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //get  all users
+    const users = await User.find();
+    return res.status(200).json({ message: "Ok", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
 };
