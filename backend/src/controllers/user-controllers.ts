ef92e6b2f87b1d28011d1bda6433dbdafde8cb67
Sudheesh T D <sudheeshtd01@gmail.com>
2024-09-12 00:09:30 -0500

    user signup controller added

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { hash } from "bcrypt";
 import User from "../models/User.js";
 
 export const getAllUsers = async (
@@ -10,6 +11,24 @@ export const getAllUsers = async (
     //get  all users
     const users = await User.find();
     return res.status(200).json({ message: "Ok", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //User Signup
+    const { name, email, password } = req.body;
+    const hashedPassword = await hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    return res.status(200).json({ message: "Ok", id: user._id.toString() });
   } catch (error) {
     console.log(error);
     return res.status(200).json({ message: "ERROR", cause: error.message });
