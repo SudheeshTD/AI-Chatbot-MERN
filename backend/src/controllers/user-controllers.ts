6044f9c31b1fd5d486b75d94a56715575513752a
Sudheesh T D <sudheeshtd01@gmail.com>
2024-09-12 10:46:35 -0500

    JWT added with Http cookie

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { hash, compare } from "bcrypt";
 import User from "../models/User.js";
+import { createToken } from "../utils/token-manager.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -53,6 +54,18 @@ export const userLogin = async (
     if (!isPasswordCorrect) {
       return res.status(403).send("Incorrect Password");
     }
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie("auth_token", token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: "Ok", id: user._id.toString() });
   } catch (error) {
     console.log(error);
